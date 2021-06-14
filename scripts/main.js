9acc8743428e18ce39d5ef85c42250950e90ed75
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2021-06-14 14:36:10 +0530

    added portfolio template from web

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = () => {changeOpacity()};
-
-function changeOpacity() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        document.documentElement.style.setProperty('--opacity', 1);
-    } else {
-        document.documentElement.style.setProperty('--opacity', 0);
-    }
-}
