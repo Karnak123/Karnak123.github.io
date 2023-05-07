2c776ec390cd692b617b84e2c0cb23c0334ff334
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2023-05-07 19:57:58 +0530

    updated about section

--- src/content_option.js
+++ src/content_option.js
@@ -9,13 +9,14 @@ const introdata = {
     animated: {
         first: "A Software Engineer with a passion for creating innovative solutions."
     },
-    description: "I work at CloudKaptan, a leading company that provides cloud-based services using Salesforce. I have been involved in developing and deploying various applications and integrations for our clients, using the latest technologies and best practices. I enjoy learning new skills and exploring new challenges, and always strive to deliver high-quality results.\nI created this portfolio page to showcase some of my work and achievements. I hope you enjoy browsing through my portfolio.",
+    description: "I work at CloudKaptan, a leading company that provides cloud-based services using Salesforce. I have been involved in developing and deploying various applications and integrations for our clients, using the latest technologies and best practices. I enjoy learning new skills and exploring new challenges, and always strive to deliver high-quality results.",
+    description1: "I created this portfolio page to showcase some of my work and achievements. I hope you enjoy browsing through my portfolio.",
     your_img_url: "https://i.imgur.com/6BNw4Tz.png",
 };
 
 const dataabout = {
     title: "a bit about my self",
-    aboutme: "I have a background in Computer Science and Engineering, and have worked on projects involving web development, data analysis, machine learning, and natural language processing. I have also participated in several hackathons and coding competitions, where I showcased my problem-solving and teamwork abilities.",
+    aboutme: "I am a software engineer with experience in developing and maintaining complex and intricate systems and integrations for various user flows and business requirements. Some of the major systems that I have designed and implemented include a loan amortization calculator and NACHA for funds transfer. My goal is to advance my career to a more managerial role where I can oversee the architecture and implementation of whole systems and integrations.",
 };
 const worktimeline = [{
     jobtitle: "Software Engineer",
@@ -31,11 +32,6 @@ const worktimeline = [{
     jobtitle: "Cultural Secretary",
     where: "GCETT-B Students' Association",
     date: "Nov 2021 - June 2022",
-},
-{
-    jobtitle: "Database Developer",
-    where: "Govt. College of Engineering and Textile Technology, Berhampore",
-    date: "Oct 2018 - Jan 2019",
 },
 ];
 
@@ -53,13 +49,7 @@ const skills = [{
 },
 {
     name: "Salesforce",
-    value: 60,
-},
-];
-
-const services = [{
-    title: "Salesforce",
-    description: "Been working with Salesforce for nearly a year. Having developed a fintech product from scratch to production, have gained a fair understanding of the platform.",
+    value: 75,
 },
 ];
 
@@ -105,7 +95,6 @@ export {
     dataportfolio,
     worktimeline,
     skills,
-    services,
     introdata,
     contactConfig,
     socialprofils,
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -37,6 +37,7 @@ export const Home = () => {
                   />
                 </h1>
                 <p className="mb-1x">{introdata.description}</p>
+                <p className="mb-1x">{introdata.description1}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
                     <div id="button_p" className="ac_btn btn ">
